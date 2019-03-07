900c638d9d81b19da695f7a66e45584655cc676c
gafeol <gabfeol@gmail.com>
2019-03-07 15:56:15 -0300

    Updates

---
+++ precache-manifest.61c4a0fcaee98f2a212dca5269195c51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ed764eef3d40f5a63964de2634ba930",
+    "url": "/minesweeper/static/media/bomb.4ed764ee.png"
+  },
+  {
+    "revision": "ffe58712ac7260b0046e",
+    "url": "/minesweeper/static/js/runtime~main.52e6a517.js"
+  },
+  {
+    "revision": "3acb9592983445d09171",
+    "url": "/minesweeper/static/js/main.50b8b880.chunk.js"
+  },
+  {
+    "revision": "7488cdef97475e8e5245",
+    "url": "/minesweeper/static/js/2.9f1434f6.chunk.js"
+  },
+  {
+    "revision": "3acb9592983445d09171",
+    "url": "/minesweeper/static/css/main.65888d4b.chunk.css"
+  },
+  {
+    "revision": "7488cdef97475e8e5245",
+    "url": "/minesweeper/static/css/2.b7561250.chunk.css"
+  },
+  {
+    "revision": "67d21fec35a8c1c438778faf73e1a7cc",
+    "url": "/minesweeper/index.html"
+  }
+];
